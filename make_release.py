77ceb84f3493c1cb7a37bdddd420442be2a0a76c
George Notaras <gnot@g-loaded.eu>
2012-05-11 14:54:35 +0300

    Updated release script.

--- make_release.py
+++ make_release.py
@@ -12,10 +12,35 @@ REL_FILES = [
 
 PLUGIN_METADATA_FILE = 'cc-configurator.php'
 
+POT_HEADER = """#  POT (Portable Object Template)
+#
+#  This file is part of the Creative-Commons-Configurator plugin for WordPress.
+#
+#  http://www.g-loaded.eu/2006/01/14/creative-commons-configurator-wordpress-plugin/
+#
+#  Copyright (C) 2006-2012 George Notaras <gnot@g-loaded.eu>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+"""
+
+# ==============================================================================
+
 import sys
 import os
 import zipfile
 import shutil
+import subprocess
 
 def get_name_release():
 	def get_data(cur_line):
@@ -43,6 +68,29 @@ def get_name_release():
 		return name, release
 
 name, release = get_name_release()
+
+
+
+# Translation
+pot_domain = os.path.splitext(PLUGIN_METADATA_FILE)[0]
+
+# Generate POT file
+args = ['xgettext', '--default-domain=%s' % pot_domain, '--output=%s.pot' % pot_domain, '--language=PHP', '--from-code=UTF-8', '--keyword=__', '--keyword=_e', '--no-wrap', '--package-name=%s' % pot_domain, '--package-version=%s' % release, '--copyright-holder', 'George Notaras <gnot@g-loaded.eu>', '%s.php' % pot_domain]
+p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = p.communicate()
+
+# Replace POT Header
+
+f = open('%s.pot' % pot_domain, 'r')
+pot_lines = f.readlines()
+f.close()
+f = open('%s.pot' % pot_domain, 'w')
+f.write(POT_HEADER)
+for n, line in enumerate(pot_lines):
+    if n < 4:
+        continue
+    f.write(line)
+f.close()
 
 # Because of the stupidity of the WordPress plugin registration system
 # the plugin directory ends with ``-1``
